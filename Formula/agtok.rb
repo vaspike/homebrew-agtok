43e5b0d990e11d99bf1b7e24a1204a9c258703b0
River Mao <vaspike@outlook.com>
2025-11-02 02:54:34 +0800

    v0.1.1

--- Formula/agtok.rb
+++ Formula/agtok.rb
@@ -6,11 +6,11 @@ class Agtok < Formula
   on_macos do
     on_intel do
       url "https://github.com/vaspike/agtok/releases/download/0.1.1/agtok-v0.1.1-darwin-amd64.tar.gz"
-      sha256:9ba794c830571373ab8c23c9ef4e1cf51e94d615b01890310ec95033108f8c1e
+      sha256 "9ba794c830571373ab8c23c9ef4e1cf51e94d615b01890310ec95033108f8c1e"
     end
     on_arm do
       url "https://github.com/vaspike/agtok/releases/download/0.1.1/agtok-v0.1.1-darwin-arm64.tar.gz"
-      sha256:03114271c847ef5edc9aa77d0d6316b3c3cab1ca7473000e268019a2a7e48345
+      sha256 "03114271c847ef5edc9aa77d0d6316b3c3cab1ca7473000e268019a2a7e48345"
     end
   end
 
