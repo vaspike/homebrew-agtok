6a88abe5ec5e50d5925ef75e90ca66ae2380eca6
River Mao <vaspike@outlook.com>
2025-11-07 21:27:40 +0800

    Update agtok.rb

--- Formula/agtok.rb
+++ Formula/agtok.rb
@@ -1,16 +1,16 @@
 class Agtok < Formula
   desc "AI Agent Token Control (TUI + CLI)"
   homepage "https://github.com/vaspike/agtok"
-  version "0.1.4"
+  version "1.0.1"
 
   on_macos do
     on_intel do
       url "https://github.com/vaspike/agtok/releases/download/#{version}/agtok-v#{version}-darwin-amd64.tar.gz"
-      sha256 "98f691f5bd17461e1b84f02ad0d5d01ed8a7d11a20636f7582e00fe8596f9af7"
+      sha256 "044e36ec937e4933d46d59ff98fc16aaa663b3a1d566539bd0d7133028972c53"
     end
     on_arm do
       url "https://github.com/vaspike/agtok/releases/download/#{version}/agtok-v#{version}-darwin-arm64.tar.gz"
-      sha256 "fb672e75f948cc84e6fd8fe1d7beb7b14eb684a5ec09b96b7a586a8f9c48ec70"
+      sha256 "738801d90a786d76b2af32f5d6048a47048e76ba250bee6b683b71feb8297518"
     end
   end
 
